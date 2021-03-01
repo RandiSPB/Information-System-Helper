43a59a7f3f774071c08a779f6698229cafcc04d6
RandiSPB <i.tsirulik@sellwell.ru>
2021-03-01 12:47:35 +0300

    add cp886 - utf8 decoder and memory cost func

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def message_info(x: float, base: int = 2) -> float:
     return round(-1 * math.log(x, base), 5)
 
 
-probability_distribution = (() for i in range(16))
-N = 256
-V = math.log(N, 2)
-v = 160 * 192
-print(v * V / 8 / 1024)
+def text_memory_coast(alphabet_power: int, num_pages: int, chars_per_page: int, measure_unit: str = 'bit',
+                      order: str = '') -> str:
+    units_map = {'bit': 1, 'byte': 8}
+    order_map = {'': 1, 'kilo': 2 ** 10, 'mega': 2 ** 20, 'giga': 2 ** 30, 'tera': 2 ** 40}
+    memory_cost = int(num_pages * chars_per_page * math.log(alphabet_power, 2))
+    return f'Memory cost: {round(memory_cost  / units_map[measure_unit] / order_map[order], 5)} {order}{measure_unit}'
+
