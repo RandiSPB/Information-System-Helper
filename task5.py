948ab92a0680a13de96602cf0395481ea496d832
RandiSPB <60474777+RandiSPB@users.noreply.github.com>
2021-04-23 05:39:30 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,66 @@
+from task2 import convert_fib, convert_to_bin, convert_to_bin_rev
+from typing import Tuple
+import math
+
+
+def un_code(number: int, code_type: str = "direct") -> str:
+    res = ''
+    number_type = {'inverse': '0', 'direct': '1'}
+    for i in range(number):
+        res += number_type[code_type]
+    if code_type == 'inverse':
+        return res + '1'
+    else:
+        return res + '0'
+
+
+def fib_code(number: int) -> str:
+    return convert_fib(number)[::-1] + '1'
+
+
+def gamma_lev(number: int) -> str:
+    bin_form = convert_to_bin(number)[::-1]
+    tmp = ''
+    for i in range(len(bin_form) - 1):
+        tmp += f'0{bin_form[i]}'
+    return tmp + bin_form[-1]
+
+
+def get_limits(number: int) -> Tuple[int, int]:
+    min_l = 0
+    max_l = 1
+    while True:
+        if (number >= 2 ** min_l) and (number <= 2 ** max_l - 1):
+            return tuple((min_l, max_l))
+        else:
+            min_l += 1
+            max_l += 1
+
+
+def gamma_el(number: int) -> str:
+    limits = get_limits(number)
+    return un_code(limits[0], 'inverse') + convert_to_bin(int(number - 2 ** limits[0]))
+
+
+def del_el(number: int) -> str:
+    bin_form = convert_to_bin(number)
+    l = len(bin_form)
+    l_bin = convert_to_bin(int(l))
+    m = len(l_bin)
+    return '0' * (m - 1) + '1' + l_bin[1::] + bin_form[1::]
+
+
+def omega_el(number: int) -> str:
+    res = '0'
+    while number != 1:
+        bin_form = convert_to_bin(number)
+        res = bin_form + res
+        number = len(bin_form) - 1
+    return res
+
+
+print(gamma_el(100))
+print(omega_el(15))
+print(del_el(34))
+
+
